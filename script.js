6e3f7a14b7afc20c18f8229afbfa4acda3ee72fb
Ranganivandan <147639923+Ranganivandan@users.noreply.github.com>
2023-10-27 21:00:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let signup=document.getElementById("signupbtn");
+let signin=document.getElementById("signinbtn");
+let title=document.getElementById("title");
+let namefield=document.getElementById("name-field");
+
+let flag=0;
+// function signinn(){
+    
+// document.getElementById("title").innerHTML = "Sign-in";
+// document.getElementById("name-field").style.display="none"
+// }
+
+// if(flag==0){
+    function signinn(){
+        
+        document.getElementById("title").innerHTML = "Sign-in";
+        document.getElementById("name-field").style.display="none"
+        // flag=1;
+        // console.log("1"); 
+    }
+    
+
+
+    function signupp(){
+        document.getElementById("title").innerHTML="Sign up"
+        document.getElementById("name-field").style.display="block"
+        }
+       
+
